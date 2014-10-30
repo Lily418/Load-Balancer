f3e4ebdebdae016662b3bf28648d3bda9039c954
Lily Hoskin <lilyhoskin@gmail.com>
2014-10-30 13:15:28 +0000

    Added round-robin load balancer to the LoadPredictor

--- CPUMonitor/main.js
+++ CPUMonitor/main.js
@@ -5,6 +5,14 @@ var getCpuUsage = require("./top.js");
 
 var run = true;
 
+http.listen(3005, function(){
+  console.log('listening on *:3005');
+});
+
+app.get('/', function(req, res){
+    res.end("Here is a website " + Math.random());
+});
+
 console.log('connecting')
 var socket = io.connect('http://178.62.27.130:3000');
 
