692cd0baa073f4327a42ee7201a845717fbc777c
jxh245 <jxh245@d27c35ba-b5f6-4602-adfd-c9b2eda2e5a3>
2014-10-30 15:31:18 +0000

    Added round-robin load balancer to the LoadPredictor
    git-svn-id: https://codex.cs.bham.ac.uk/svn/projects/2014/jxh245@6 d27c35ba-b5f6-4602-adfd-c9b2eda2e5a3

--- LoadPredictor/main.js
+++ LoadPredictor/main.js
@@ -1,22 +1,50 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var request = require("request");
 
+
+//var connectedServers = {}
+var serverQueue = []
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
+//Returns the IP of the next server in the round-robin
+function nextServer(){
+    var ip = serverQueue.shift();
+    serverQueue.push(ip);
+    return ip;
+}
+
+app.get('/', function(req, res){
+    var server = nextServer();
+    request("http://" + server + ":3005" , function(error, response, body) {
+        res.write("You were served by " + server + "\n")
+        res.end(body);
+    })
+});
+
 io.on('connection', function(socket){
   console.log(socket.request.connection._peername.address);
+  var ip = socket.request.connection._peername.address;
 
   socket.on('cpu-usage', function(msg){
-    console.log(msg)
-    io.emit('cpu-ip', JSON.stringify({ip: socket.request.connection._peername.address,
+    if(serverQueue.indexOf(ip) === -1){
+        serverQueue.push(ip);
+    }
+    
+    //connectedServers[ip] = {time: new Date().getTime()};
+    io.emit('cpu-ip', JSON.stringify({ip: ip,
                                 usage: msg}));
   });
 
   socket.on('disconnect', function(){
     console.log('user disconnected');
+    var index = serverQueue.indexOf(ip);
+    if(index > -1){
+        serverQueue.splice(index, 1);
+    }
   });
 });
