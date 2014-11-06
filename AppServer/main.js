01c5ac84e7c8353ffac919dd72f6fb1333e9ba12
Lily Hoskin <lilyhoskin@gmail.com>
2014-11-06 21:45:40 +0000

    Seperated CPUMonitor from AppServer so they can be run as seperate processes so I can use nice to ensure CPUMonitor continues to run

---
+++ AppServer/main.js
@@ -0,0 +1,19 @@
+var app = require('express')();
+var http = require('http').Server(app);
+
+http.listen(3005, function(){
+  console.log('listening on *:3005');
+});
+
+//Arbitary calculation to simulate load when user makes a request
+function doCalculations(){
+    var x = 0;
+    for(var i = 0; i < Math.pow(10, 8) * 5; i++){
+        x += Math.random();
+    }
+    return x;
+}
+
+app.get('/', function(req, res){
+    res.end("Here is a website " + doCalculations());
+});
--- CPUMonitor/main.js
+++ CPUMonitor/main.js
@@ -1,28 +1,5 @@
-var app = require('express')();
-var http = require('http').Server(app);
 var io = require('socket.io-client');
 var getCpuUsage = require("./top.js");
-
-var run = true;
-
-http.listen(3005, function(){
-  console.log('listening on *:3005');
-});
-
-//Arbitary calculation to simulate load when user makes a request
-function doCalculations(){
-    var x = 0;
-    for(var i = 0; i < Math.pow(10, 8) * 5; i++){
-        x += Math.random();
-    }
-    return x;
-}
-
-app.get('/', function(req, res){
-    res.end("Here is a website " + doCalculations());
-});
-
-
 
 console.log('connecting')
 var socket = io.connect('http://192.168.56.101:3000');
