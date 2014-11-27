ac533b7ce1081a37ebc022d4e0ecbe57af105a3f
Lily Hoskin <lilyhoskin@gmail.com>
2014-11-27 23:42:55 +0000

    Reduced complexity of app server calculations to fit in better with the better than realtime simulation

--- AppServer/main.js
+++ AppServer/main.js
@@ -8,7 +8,7 @@ http.listen(3005, function(){
 //Arbitary calculation to simulate load when user makes a request
 function doCalculations(){
     var x = 0;
-    for(var i = 0; i < Math.pow(10, 8) * 5; i++){
+    for(var i = 0; i < Math.pow(10, 8); i++){
         x += Math.random();
     }
     return x;
