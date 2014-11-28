fdc8b7950ea2a6fe99e69372af75e5178d88d6a2
jxh245 <jxh245@d27c35ba-b5f6-4602-adfd-c9b2eda2e5a3>
2014-11-28 01:26:41 +0000

    Created controller for virtual machines
    git-svn-id: https://codex.cs.bham.ac.uk/svn/projects/2014/jxh245@14 d27c35ba-b5f6-4602-adfd-c9b2eda2e5a3

--- CloudController/cloud.js
+++ CloudController/cloud.js
@@ -3,7 +3,7 @@ var exec = require('child_process').exec;
 module.exports = {
     createNewServer: function(){
         var vmName = new Date().getTime();
-        var createVm = exec('VBoxManage clonevm cd37efcb-9af4-4b90-a008-9ddc22e84006 --options link --snapshot 83c00d8e-36a4-433e-9cdd-f39787efef41 --register --name ' + vmName);
+        var createVm = exec('VBoxManage clonevm cd37efcb-9af4-4b90-a008-9ddc22e84006 --options link --snapshot 70b1f605-f17b-4075-818e-bc7f9fd4f679 --register --name ' + vmName);
         createVm.on('exit', function(){
             var startvm = exec('VBoxManage startvm ' + vmName + " --type headless");
         });
