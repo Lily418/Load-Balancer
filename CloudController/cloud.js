6cc7a1f0f0525fed4d61beeede0d68b4b0e5360c
Lily Hoskin <lilyhoskin@gmail.com>
2014-11-28 01:24:21 +0000

    Changed AppServer, modified CloudController to use correct snapshot

--- CloudController/cloud.js
+++ CloudController/cloud.js
@@ -3,7 +3,7 @@ var exec = require('child_process').exec;
 module.exports = {
     createNewServer: function(){
         var vmName = new Date().getTime();
-        var createVm = exec('VBoxManage clonevm cd37efcb-9af4-4b90-a008-9ddc22e84006 --options link --snapshot 70b1f605-f17b-4075-818e-bc7f9fd4f679 --register --name ' + vmName);
+        var createVm = exec('VBoxManage clonevm cd37efcb-9af4-4b90-a008-9ddc22e84006 --options link --snapshot 71dfb56e-76b9-42a8-b51e-6271c84db2ce --register --name ' + vmName);
         createVm.on('exit', function(){
             var startvm = exec('VBoxManage startvm ' + vmName + " --type headless");
         });
