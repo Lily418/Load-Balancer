b954381d3c772ae654f538d3af4803fc1db6a964
Lily Hoskin <lilyhoskin@gmail.com>
2014-12-07 19:53:18 +0000

    Refactored scale.js

--- LoadPredictor/scale.js
+++ LoadPredictor/scale.js
@@ -9,9 +9,26 @@ socket.on('cpu-ip', function(msg){
 
 var requestsHandledPerInterval = 20
 
-module.exports = function(redisClient, timeInterval, io){
-    redisClient.get("training:requests:" + timeInterval, function(err, value){
+function calculateOptimalServers(timeInterval, seriesTag, callback){
+    redisClient.get(seriesTag + ":requests:" + timeInterval, function(err, value){
         var optimalServers = Math.ceil(value / requestsHandledPerInterval);
-        io.emit('optimal-servers', JSON.stringify({"timeInterval": timeInterval, "optimalServers": optimalServers}));
+        callback(timeInterval, optimalServers);
     });
+}
+
+function createEmitOptimal(io){
+    return function(timeInterval, optimalServers){
+        io.emit('optimal-servers', JSON.stringify({"timeInterval": timeInterval, "optimalServers": optimalServers}));
+    }
+}
+
+module.exports = function(redisClient, timeInterval, io){
+    calculateOptimalServers(timeInterval, "training", createEmitOptimal(io));
+
+    /*var prevInterval = timeInterval - (10 * 1000);
+    //Calculate true optimal for previous timeframe
+    redisClient.get("testing:requests:" + prevInterval, function(err,value){
+        var optimalServers = Math.ceil(value / requestsHandledPerInterval);
+        io.emit('optimal-servers', JSON.stringify({"timeInterval": timeInterval, "optimalServers": optimalServers}))
+    });*/
 }
