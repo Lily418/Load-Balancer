0c6cb3d46f9c2339972e6597c12634de9ba47872
Lily Hoskin <lilyhoskin@gmail.com>
2015-02-04 21:29:26 +0000

    Fix module syntax again

--- LoadPredictor/linear_regression.js
+++ LoadPredictor/linear_regression.js
@@ -6,7 +6,7 @@ var w2 = 0;
 module.exports = {
     addData : function(d){
         data.push(d);
-    }
+    },
 
     predict : function(tMinusOneYear, tMinusOneHour){
         return Math.ceil((w1 * tMinusOneYear) + (w2 * tMinusOneHour) + w0);
