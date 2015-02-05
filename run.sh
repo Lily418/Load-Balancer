bf18754913911beeed70d8b155724212ee71a668
Lily Hoskin <lilyhoskin@gmail.com>
2015-02-05 14:50:21 +0000

    Updated the run script

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
-redis-server --dir DataCollection/ &
-node CloudController/main.js &
-(cd Visualisation && python -mSimpleHTTPServer) &
-open -a "/Applications/Google Chrome.app" 'http://127.0.0.1:8000/'
+redis-server --dir DataCollection/ > redis_out.txt &
+node CloudController/main.js  > cloud_controller_out.txt &
+python -mSimpleHTTPServer > http_server_out.txt &
+open -g -a "/Applications/Google Chrome.app" 'http://127.0.0.1:8000/Visualisation'
 echo Start Load Predictor on VM now
 read
-python3 LoadSimulator/main.py
+python3 LoadSimulator/main.py > load_simulator_out.txt &
+echo "Input to end"
+read
+pkill -f "python -mSimpleHTTPServer"
+pkill -f "LoadSimulator/main.py"
+pkill -f "node CloudController/main.js"
+pkill -f "redis-server"
